99216332e7c40f59a726ffe22c410d4ffd517382
Sneha Patne <snehapatne2311@gmail.com>
2025-10-26 13:11:08 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
+package src;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("?? Hello, Jenkins + Docker World!");
+        System.out.println("👋 Hello, Jenkins + Docker World!");
     }
 }
+
